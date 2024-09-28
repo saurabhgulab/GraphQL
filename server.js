bef4cdebd4f0dde9db2773fe4ed47dd1275e3435
Saurabh Krishan Gulab <saurabhgulab@gmail.com>
2024-09-28 22:27:39 +0530

    schemas created for user and quotes as well as connection established with mongoDb successfully.

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 import { ApolloServer } from "apollo-server";
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
-import resolvers from "./resolvers.js";
 import typeDefs from "./schemaGQL.js";
+import mongoose from "mongoose";
+import { MONGO_URI } from "./config.js";
+
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.on("connected", () => {
+  console.log("Connected to MongoDb");
+});
+mongoose.connection.on("error", (err) => {
+  console.log("Error connecting to MongoDb", err);
+});
+
+import "./models/Quotes.js";
+import "./models/User.js";
+
+import resolvers from "./resolvers.js";
 
 const server = new ApolloServer({
   typeDefs,
@@ -11,3 +28,5 @@ const server = new ApolloServer({
 server.listen({ port: 4000 }).then(({ url }) => {
   console.log(`Server running at ${url}`);
 });
+
+// mongodb+srv://saurabhgulab:<db_password>@cluster0.lffwt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
