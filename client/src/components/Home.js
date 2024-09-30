2eb51c95d1f244bf0a5ed4d5daf578685e16a303
Saurabh Krishan Gulab <saurabhgulab@gmail.com>
2024-09-30 19:48:51 +0530

    fetching data from mongoDb via graphQL initialised

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,16 +1,24 @@
 import React from "react";
+import { useQuery } from "@apollo/client";
+import { GET_ALL_QUOTES } from "../gqlOperations/queries";
 
 const Home = () => {
+  const { loading, error, data } = useQuery(GET_ALL_QUOTES);
+  if (loading) return;
+  <h1>loading</h1>;
+  if (error) {
+    console.log(error.message);
+  }
   return (
     <div className="container my-container">
-      <blockquote>
-        <h6>Quote 1</h6>
-        <p className="right-align">~name 1</p>
-      </blockquote>
-      <blockquote>
-        <h6>Quote 2</h6>
-        <p className="right-align">~name 2</p>
-      </blockquote>
+      {data.quotes.map((quote) => {
+        return (
+          <blockquote>
+            <h6>{quote.name}</h6>
+            <p className="right-align">{quote.by.firstName}</p>
+          </blockquote>
+        );
+      })}
     </div>
   );
 };
